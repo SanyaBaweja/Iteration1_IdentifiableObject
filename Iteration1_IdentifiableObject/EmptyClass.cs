a64b708678787d6c7a00acf9ae4d78d32a44bdeb
SanyaBaweja <bawejasanya2602@gmail.com>
2023-11-25 13:18:51 +1100

    Initial commit

---
+++ Iteration1_IdentifiableObject/EmptyClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Iteration1_IdentifiableObject
+{
+	public class Game_Object:Identifiable_object
+	{
+		private string _description;
+		private string _name;
+		public Game_Object(string[] ids, string name, string desc): base(ids)
+		{
+			_name = name;
+			_description = desc;
+		}
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+		}
+		public string ShortDescription
+		{
+			get
+			{
+				return _name + " " + FirstId;
+			}
+		}
+		public virtual string FullDescription
+		{
+			get
+			{
+				return _description;
+			}
+		}
+	}
+}
+
